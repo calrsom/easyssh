b084f81887b9eafe146eb916558aed669cafb340
beccaria <carlo.beccaria@it.equens.com>
2016-02-20 21:49:03 +0100

    add support for remote scp location

--- easyssh.go
+++ easyssh.go
@@ -151,7 +151,7 @@ func (ssh_conf *MakeConfig) Run(command string) (outStr string, err error) {
 }
 
 // Scp uploads sourceFile to remote machine like native scp console app.
-func (ssh_conf *MakeConfig) Scp(sourceFile string) error {
+func (ssh_conf *MakeConfig) Scp(sourceFile string, destDir string) error {
 	session, err := ssh_conf.connect()
 
 	if err != nil {
@@ -188,7 +188,7 @@ func (ssh_conf *MakeConfig) Scp(sourceFile string) error {
 		}
 	}()
 
-	if err := session.Run(fmt.Sprintf("scp -t %s", targetFile)); err != nil {
+	if err := session.Run(fmt.Sprintf("scp -t %s", destDir + "/" + targetFile)); err != nil {
 		return err
 	}
 
